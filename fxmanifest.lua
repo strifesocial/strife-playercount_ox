890b7b98d36790d8edce6f82ab8ade2a713c2d17
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-25 13:10:51 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Strife Social Developments'
+description 'Player Count Script'
+version '1.0'
+
+lua54 'yes'
+
+client_script 'client.lua'
+server_script 'server.lua'
